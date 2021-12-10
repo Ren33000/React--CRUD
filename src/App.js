d8f2e33582d12287332a92cc5a3c2210a9d7a865
Mylene <pont.mylene@gmail.com>
2021-12-10 19:14:59 +0900

    routes and paths created

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import './App.css';
+import React from 'react';
+import  { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import EditUser from './components/EditUser';
+import AddUser from './components/AddUser';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router> 
+        <h1>Nav</h1>
+        <Routes>
+          <Route path ="/" component= {Home} />
+          <Route path ="/add" component= {AddUser} />
+          <Route path ="/edit/:id" component= {EditUser}  />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
